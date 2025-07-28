5d03fba799c7a912b903d825c82f4e45b9d36823
FOREST <bye0414@naver.com>
2025-07-28 21:51:11 +0900

    프로젝트 생성
    디펜던시 없이 프로젝트 생성

---
+++ src/main/java/hello/study_spring_basic_repeat/StudySpringBasicRepeatApplication.java
@@ -0,0 +1,13 @@
+package hello.study_spring_basic_repeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBasicRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBasicRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/study_spring_basic_repeat/StudySpringBasicRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.study_spring_basic_repeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBasicRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
