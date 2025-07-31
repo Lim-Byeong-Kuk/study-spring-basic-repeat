d9ce156129352371039d120423646bc0ec8796f7
FOREST <bye0414@naver.com>
2025-07-31 21:52:05 +0900

    정률 할인 정책 도메인 개발 및 테스트

--- src/main/java/hello/study_spring_basic_repeat/order/OrderServiceImpl.java
+++ src/main/java/hello/study_spring_basic_repeat/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.study_spring_basic_repeat.order;
 
 import hello.study_spring_basic_repeat.discount.DiscountPolicy;
-import hello.study_spring_basic_repeat.discount.FixDiscountPolicy;
+import hello.study_spring_basic_repeat.discount.RateDiscountPolicy;
 import hello.study_spring_basic_repeat.member.Member;
 import hello.study_spring_basic_repeat.member.MemberRepository;
 import hello.study_spring_basic_repeat.member.MemoryMemberRepository;
@@ -9,7 +9,8 @@ import hello.study_spring_basic_repeat.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
