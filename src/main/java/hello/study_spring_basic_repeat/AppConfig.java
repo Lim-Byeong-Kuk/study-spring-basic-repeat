b06feff5d7baf138874f777cd2267d4ebb29c12e
FOREST <bye0414@naver.com>
2025-08-03 16:12:35 +0900

    AppConfig 리팩토링

--- src/main/java/hello/study_spring_basic_repeat/AppConfig.java
+++ src/main/java/hello/study_spring_basic_repeat/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.study_spring_basic_repeat;
 
+import hello.study_spring_basic_repeat.discount.DiscountPolicy;
 import hello.study_spring_basic_repeat.discount.FixDiscountPolicy;
+import hello.study_spring_basic_repeat.discount.RateDiscountPolicy;
+import hello.study_spring_basic_repeat.member.MemberRepository;
 import hello.study_spring_basic_repeat.member.MemberService;
 import hello.study_spring_basic_repeat.member.MemberServiceImpl;
 import hello.study_spring_basic_repeat.member.MemoryMemberRepository;
@@ -10,11 +13,19 @@ import hello.study_spring_basic_repeat.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 
